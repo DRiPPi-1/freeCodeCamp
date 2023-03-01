aa19eb29a89b73fd6f365e0fff84374c6156160a
Hlumelo Siyo <hlumelosiyo2@gmail.com>
2023-03-01 11:52:16 +0200

    Initial commit

---
+++ bartender1.js
@@ -0,0 +1,16 @@
+var your_drink
+
+var reverse = function(s) {
+    return s.split("").reverse().join("");
+}
+
+var bartender = {
+    str1: "ers",
+    str2: reverse("rap"),
+    str3: "amet",
+    request: function(preference) {
+        return preference+".secret word:"+this.str2 + this.str3 + this.str1;
+    }
+};
+
+bartender.request(your_drink);
